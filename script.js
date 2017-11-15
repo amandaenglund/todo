a1729b1ce22b7c502298c4facbe5986153b76871
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2017-11-15 20:51:50 +0100

    Login krångel

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 $(document).ready(function(){        
 
+    sessionStorage.user = "test"
+    sessionStorage.password = "password"
 
+    //syns vid start
+    $("input").show();     
+    $(".loginButton").show();     
+    $(".logoutButton").hide(); 
 
+    $("#main").show();     
+    $("#forgot").hide();     
+    $("#member").hide(); 
 
+    
+    //klicka på knapparna
+    $(".loginButton").click(function(){
+        if ( $(".user").val() == sessionStorage.user && $(".password").val() == sessionStorage.password ) {
+            // om rätt svar gör såhär
+            console.log("rättttttt! PANNKAKA!")
+            $("#member").show(); 
 
+        } else {
+            // om fel svar gör detta
+            console.log("Fel fel fel")
+            $("#forgot").show();     
+        }
 
+    });
 
-
+    $(".logoutButton").click(function(){
+        $("#main").show();     
+        
+    });
 });
 
 
-
-
-
-var stuffToDo = [
-    ”Klipp gräset", 
-    ”Betala räkningar",
-    ”Köp mjölk", 
-    ”Spika upp tavlor"
-    ];
-    
+// var stuffToDo = [
+//     ”Klipp gräset", 
+//     ”Betala räkningar",
+//     ”Köp mjölk", 
+//     ”Spika upp tavlor"
+//     ];
