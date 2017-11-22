48bcd3e4a0799e4ddb19cbed499384f114a560bc
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2017-11-22 10:44:31 +0100

    Printat arrayen på en rad

--- script.js
+++ script.js
@@ -1,43 +1,101 @@
 $(document).ready(function(){        
 
-    sessionStorage.user = "test"
-    sessionStorage.password = "password"
+    var user = "test"
+    var password = "password"
 
-    //syns vid start
-    $("input").show();     
-    $(".loginButton").show();     
-    $(".logoutButton").hide(); 
-
-    $("#main").show();     
-    $("#forgot").hide();     
-    $("#member").hide(); 
-
+    var stuffToDo = [
+        "Klipp gräset", 
+        "Betala räkningar",
+        "Köp mjölk", 
+        "Spika upp tavlor",
+        "Mata katten"
+        ];
     
-    //klicka på knapparna
+
+
+    if (sessionStorage.ourUser != null) {
+            
+        // Är vi inloggade
+        visaMember();
+        
+
+    } else {
+
+        //om vi inte är inloggade
+        visaMain();
+        
+    }
+
+    //login-knappen
     $(".loginButton").click(function(){
-        if ( $(".user").val() == sessionStorage.user && $(".password").val() == sessionStorage.password ) {
-            // om rätt svar gör såhär
-            console.log("rättttttt! PANNKAKA!")
-            $("#member").show(); 
+      
+        if ( $(".user").val() == user && $(".password").val() == password ) {
+            // om rätt svar - visa member-sida
+            visaMember();
+            
+            //printa arrayen - provat denna JQuery-kod som inte funkar
+            //$("todo").HTML = stuffToDo;
+            
+            var myJSON = JSON.stringify(stuffToDo);
+            document.getElementById("todo").innerHTML = myJSON;
+
+        
 
         } else {
-            // om fel svar gör detta
-            console.log("Fel fel fel")
-            $("#forgot").show();     
+            // om fel svar - visa forgot-sida
+            visaForgot();
         }
 
     });
 
+
+    //logout-knappen
     $(".logoutButton").click(function(){
-        $("#main").show();     
-        
+        visaMain();        
     });
+
+
+    //visar dagens datum
+    var date = new Date;
+    $(".todaysDate").append(date.getFullYear(),
+    "-", date.getMonth(), "-", date.getDate());
+
+
+
+    //LITE FUNTIONER
+    //sida som syns vid start
+    function visaMain() {
+        $("input").show();     
+        $(".loginButton").show();     
+        $(".logoutButton").hide(); 
+    
+        $("#main").show();     
+        $("#forgot").hide();     
+        $("#member").hide();
+    };
+            
+
+    //visar member-sidan
+    function visaMember() {
+        $("#main").hide();    
+        $("#forgot").hide();     
+        $("#member").show(); 
+        $(".logoutButton").show(); 
+        $(".loginButton").hide();     
+        $(".user").hide();     
+        $(".password").hide();
+        $(".namn").text($(".user").val());  
+        sessionStorage.ourUser = $(".user").val();                   
+    };
+
+
+    //visar fel lösen-sidan
+    function visaForgot() {
+        $("#forgot").show(); 
+        $("#main").hide();  
+        $("#member").hide();    
+    };
+
 });
 
 
-// var stuffToDo = [
-//     ”Klipp gräset", 
-//     ”Betala räkningar",
-//     ”Köp mjölk", 
-//     ”Spika upp tavlor"
-//     ];
